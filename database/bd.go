e97f792dedfa6a51ee2001fa9d56cf0a1bde09de
Vinicius Ribas <vinicius_batista@gec.inatel.br>
2023-11-13 18:39:02 -0300

    adicionando testes mock

--- database/bd.go
+++ database/bd.go
@@ -8,12 +8,7 @@ import (
 	"gorm.io/gorm"
 )
 
-var (
-	DB  *gorm.DB
-	err error
-)
-
-func Conecta_BD() {
+func Conecta_BD() DBInterface {
 	sdc := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable "
 	//String de conexao
 	DB, err = gorm.Open(postgres.Open(sdc))
@@ -21,4 +16,59 @@ func Conecta_BD() {
 		log.Panic("erro ao conectar ao banco de dados")
 	}
 	DB.AutoMigrate(&models.Aluno{})
+	return &Database{DB: DB}
 }
+
+type DBInterface interface {
+	Find(dest interface{}) DBInterface
+	First(dest interface{}, conds ...interface{}) DBInterface
+	Create(value interface{}) DBInterface
+	Delete(value interface{}, conds ...interface{}) DBInterface
+	Model(value interface{}) DBInterface
+	UpdateColumns(value interface{}) DBInterface
+	Where(value interface{}, args ...interface{}) DBInterface
+}
+
+type Database struct {
+	*gorm.DB
+}
+
+func (db *Database) Find(dest interface{}) DBInterface {
+	db.DB.Find(dest)
+	return db
+}
+
+func (db *Database) First(dest interface{}, conds ...interface{}) DBInterface {
+	db.DB.First(dest, conds...)
+	return db
+}
+
+func (db *Database) Create(value interface{}) DBInterface {
+	db.DB.Create(value)
+	return db
+}
+
+func (db *Database) Delete(value interface{}, conds ...interface{}) DBInterface {
+	db.DB.Delete(value, conds...)
+	return db
+}
+
+func (db *Database) Model(value interface{}) DBInterface {
+	db.DB.Model(value)
+	return db
+}
+
+func (db *Database) UpdateColumns(value interface{}) DBInterface {
+	db.DB.UpdateColumns(value)
+	return db
+}
+
+func (db *Database) Where(value interface{}, args ...interface{}) DBInterface {
+	db.DB.Where(value, args...)
+	return db
+}
+
+var (
+	DB  *gorm.DB
+	err error
+)
